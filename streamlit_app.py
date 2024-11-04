a48a10840632707301d2d6d51d7f10c6c09f4245
artazurewebdev <mmais2342@gmail.com>
2024-11-04 22:53:59 +0000

    VoteInclusive2024

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.markdown(
+    """
+    <style>
+    /* Background color */
+    .stApp {
+        background-color: #222f62;
+    }
+
+    /* Sidebar background color */
+    .css-1d391kg {
+        background-color: #0e1117;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
 )
+
+st.title("VoteInclusive 2024")
+
+st.sidebar.title("VoteInclusive 2024")
+st.sidebar.subheader("Everybody deserves a vote")
+
+
